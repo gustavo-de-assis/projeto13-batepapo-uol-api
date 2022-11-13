ac81eb5ab03ec37939ded1862bb15afb56593052
gustavo de assis <gustavo.siqueira.421@gmail.com>
2022-11-13 20:07:58 -0300

    usando try/catch nas requisições

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ app.use(express.json());
 const mongoClient = new MongoClient("mongodb://localhost:27017");
 let db;
 
+
 mongoClient
     .connect()
     .then(() => {
@@ -17,37 +18,38 @@ mongoClient
     })
     .catch((err) => { console.log(err) });
 
-app.get("/participants", (req, res) => {
-    db.collection("participants")
-    .find()
-    .toArray()
-    .then((part) =>{
-        res.send(part).status(200)
-        }
-    )
-    .catch((err)=>{
-        res.sendStatus(500);
-        }
-    )
+
+app.get("/participants", async (req, res) => {
+    try {
+        const participants = await db.collection.find().toArray()
+        res.send(part).status(200);
+    }
+    catch (err) {
+        res.sendStatus(500)
+    }
 })
 app.post("/participants", (req, res) => {
 
 })
-app.get("/messages", (req, res) => {
-    db.collection("messages")
-    .find()
-    .toArray()
-    .then((msg) =>{
-        res.send(msg).status(200)
-        }
-    )
-    .catch((err)=>{
-        res.sendStatus(500);
-        }
-    )
-})
-app.post("/messages", (req, res) => {
+app.get("/messages", async (req, res) => {
+    try {
+        const messages = await db.collection("messages").find().toArray()
+        res.send(messages).status(200)
 
+    }
+    catch (err) {
+        res.sendStatus(500)
+    }
+})
+app.post("/messages", async (req, res) => {
+    try {
+        const message = req.body;
+        await db.collection("messages").insertOne(message)
+        res.status(201).send("Success! Msg sent!")
+    }
+    catch(err) {
+        res.status(500).send("Error! Couldn't send message!")
+    }
 })
 app.post("/status", (req, res) => {
 
