0f23cb28416435db224595671bea61c98b35c4b2
gustavo de assis <gustavo.siqueira.421@gmail.com>
2022-11-13 19:13:56 -0300

    get-participants v0.8

--- src/index.js
+++ src/index.js
@@ -1,23 +1,45 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
+import joi from "joi";
 
 const app = express();
-
 app.use(cors());
+app.use(express.json());
 
-app.get("/participants",(req,res)=>{
+const mongoClient = new MongoClient("mongodb://localhost:27017");
+let db;
+
+mongoClient
+    .connect()
+    .then(() => {
+        db = mongoClient.db("batepapo_uol")
+    })
+    .catch((err) => { console.log(err) });
+
+app.get("/participants", (req, res) => {
+    db.collection("participants")
+    .find()
+    .toArray()
+    .then((participants) =>{
+        res.send(participants).status(200)
+        }
+    )
+    .catch((err)=>{
+        res.sendStatus(500);
+        }
+    )
+})
+app.post("/participants", (req, res) => {
 
 })
-app.post("/participants",(req,res)=>{
+app.get("/messages", (req, res) => {
 
 })
-app.get("/messages",(req,res)=>{
+app.post("/messages", (req, res) => {
 
 })
-app.post("/messages",(req,res)=>{
-
-})
-app.post("/status",(req,res)=>{
+app.post("/status", (req, res) => {
 
 })
 app.listen(5000);
