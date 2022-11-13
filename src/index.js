74ecd5b4db8b4cebab391bed86eb46821dfd17e0
gustavo de assis <gustavo.siqueira.421@gmail.com>
2022-11-13 18:32:26 -0300

    criacao dos tipos de requisicao

--- src/index.js
+++ src/index.js
@@ -5,5 +5,19 @@ const app = express();
 
 app.use(cors());
 
+app.get("/participants",(req,res)=>{
 
+})
+app.post("/participants",(req,res)=>{
+
+})
+app.get("/messages",(req,res)=>{
+
+})
+app.post("/messages",(req,res)=>{
+
+})
+app.post("/status",(req,res)=>{
+
+})
 app.listen(5000);
