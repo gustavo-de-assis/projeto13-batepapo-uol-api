dc5bbad1b66ac30d07e8d2d25c6284bc35ae0ffa
gustavo de assis <gustavo.siqueira.421@gmail.com>
2022-11-13 23:14:26 -0300

    dotenv, requisicoes async/await e schemas

--- src/index.js
+++ src/index.js
@@ -2,14 +2,29 @@ import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
 import joi from "joi";
+import dotenv from "dotenv";
 
 const app = express();
+
+dotenv.config();
+
+
+
 app.use(cors());
 app.use(express.json());
 
-const mongoClient = new MongoClient("mongodb://localhost:27017");
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
+const userSchema = joi.object({
+    name: joi.string().required(),
+})
+
+const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().valid('message','private_message').required()
+})
 
 mongoClient
     .connect()
@@ -21,16 +36,35 @@ mongoClient
 
 app.get("/participants", async (req, res) => {
     try {
-        const participants = await db.collection.find().toArray()
-        res.send(part).status(200);
+        const participants = await db.collection("participants").find().toArray()
+        res.send(participants).status(200);
     }
     catch (err) {
         res.sendStatus(500)
     }
 })
-app.post("/participants", (req, res) => {
 
+app.post("/participants", async (req, res) => {
+    const participant = req.body;
+    
+    console.log(participant);
+
+    
+    /* const signedUser = await db.participants.findOne(participant);
+    if(signedUser){
+        res.status(409).send("User already exists");
+        return;
+    } */
+   
+    try {
+        await db.collection("participants").insertOne(participant)
+        res.status(201).send("Success! User logged in!")
+    }
+    catch(err) {
+        res.status(500).send("Error! Couldn't create User!")
+    }
 })
+
 app.get("/messages", async (req, res) => {
     try {
         const messages = await db.collection("messages").find().toArray()
@@ -41,9 +75,18 @@ app.get("/messages", async (req, res) => {
         res.sendStatus(500)
     }
 })
+
 app.post("/messages", async (req, res) => {
+    const message = req.body;
+    
+    const validation = messageSchema.validate(message, { abortEarly: true });
+    
+    if(validation.error){
+        res.sendStatus(422);
+        return;
+    }
+    
     try {
-        const message = req.body;
         await db.collection("messages").insertOne(message)
         res.status(201).send("Success! Msg sent!")
     }
@@ -51,7 +94,9 @@ app.post("/messages", async (req, res) => {
         res.status(500).send("Error! Couldn't send message!")
     }
 })
+
 app.post("/status", (req, res) => {
 
 })
-app.listen(5000);
+
+app.listen(5000, ()=>{console.log("Server running at port 5000")});
